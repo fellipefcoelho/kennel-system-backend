0a8297e9d11f23efeac5992edff3257ef8a4fb33
fellipe coelho <fellipefcoelho@gmail.com>
2022-02-04 17:17:04 -0300

    Initial Commit

---
+++ src/main/java/com/fellipe/kennelsystem/KennelSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fellipe.kennelsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KennelSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KennelSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fellipe/kennelsystem/KennelSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fellipe.kennelsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KennelSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
